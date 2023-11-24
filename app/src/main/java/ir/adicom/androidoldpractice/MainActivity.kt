5e1c15e4cb9b63eeba4c1d37848eb47eb12ce0b9
adicomdotir <adicom.ir@gmail.com>
2023-11-24 12:17:09 +0330

    Init commit

---
+++ app/src/main/java/ir/adicom/androidoldpractice/MainActivity.kt
@@ -0,0 +1,11 @@
+package ir.adicom.androidoldpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
