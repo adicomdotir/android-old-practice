69ef2329ec783252a363ad0704b289ea412fb585
adicomdotir <adicom.ir@gmail.com>
2023-11-24 14:08:24 +0330

    Add ui designs

---
+++ app/src/main/java/ir/adicom/androidoldpractice/AddNoteActivity.kt
@@ -0,0 +1,11 @@
+package ir.adicom.androidoldpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_note)
+    }
+}
